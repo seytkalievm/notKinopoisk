ba63bf1a49fb0bf3013e35a8d24573f233f470ef
Makshe Seytkaliev <3meuka0@gmail.com>
2023-02-04 16:29:18 +0300

    added FilmDetails Fragment

---
+++ app/src/main/java/com/seytkalievm/tinkoffjunlab/presentation/details/FilmDetailsFragment.kt
@@ -0,0 +1,67 @@
+package com.seytkalievm.tinkoffjunlab.presentation.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.seytkalievm.tinkoffjunlab.R
+import com.seytkalievm.tinkoffjunlab.data.model.FilmDetails
+import com.seytkalievm.tinkoffjunlab.databinding.FragmentFilmDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FilmDetailsFragment : Fragment() {
+
+    private val viewModel: FilmDetailsViewModel by viewModels()
+    private lateinit var binding: FragmentFilmDetailsBinding
+    private val args: FilmDetailsFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFilmDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val id = args.id
+        val detailsLayout = binding.filmDetails
+        detailsLayout.visibility = View.GONE
+
+        val shimmerLayout = binding.shimmerLayout
+        shimmerLayout.startShimmer()
+
+        viewModel.getFilmDetails(id)
+
+        viewModel.filmDetails.observe(viewLifecycleOwner) {
+            shimmerLayout.stopShimmer()
+            shimmerLayout.visibility = View.GONE
+            bindFilmDetails(it)
+            binding.filmDetails.visibility = View.VISIBLE
+
+        }
+
+    }
+
+    private fun bindFilmDetails(details: FilmDetails) {
+        binding.filmDetailsLayout.apply {
+            Glide.with(binding.root.context)
+                .load(details.posterUrl)
+                .placeholder(R.drawable.ic_baseline_hide_image_24)
+                .into(filmDetailsPosterIv)
+            filmDetailsTitleTv.text = details.nameRu
+            filmDetailsDescriptionTv.text = details.description
+            filmDetailsCountryTv.text = details.countries.joinToString(", ")
+            filmDetailsGenresTv.text = details.genres.joinToString(", ")
+            filmDetailsYearTv.text = details.year.toString()
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/seytkalievm/tinkoffjunlab/presentation/details/FilmDetailsViewModel.kt
@@ -0,0 +1,29 @@
+package com.seytkalievm.tinkoffjunlab.presentation.details
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.seytkalievm.tinkoffjunlab.data.model.FilmDetails
+import com.seytkalievm.tinkoffjunlab.domain.useCase.GetFilmDetailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class FilmDetailsViewModel @Inject constructor(
+    private val getFilmDetailsUseCase: GetFilmDetailsUseCase
+): ViewModel() {
+    private val _filmDetails = MutableLiveData<FilmDetails>()
+    val filmDetails: LiveData<FilmDetails> get() = _filmDetails
+
+    fun getFilmDetails(id: Int) {
+        viewModelScope.launch (Dispatchers.IO) {
+            delay(2000)
+            _filmDetails.postValue(getFilmDetailsUseCase.invoke(id))
+        }
+    }
+}
